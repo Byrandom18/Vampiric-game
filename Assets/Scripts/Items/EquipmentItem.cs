3713c655c63000a33ce65092cf6cf127a87b412c
Byrandom18 <byrandom18@mail.ru>
2025-08-01 23:52:07 +0500

    items preparations

---
+++ Assets/Scripts/Items/EquipmentItem.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "EquipmentItem", menuName = "Scriptable Objects/EquipmentItem")]
+public class EquipmentItem : ScriptableObject
+{
+    public enum Rarity { Common, Rare, Epic, Legendary }
+
+    [Header("Base Info")]
+    public string itemName;
+    public Sprite icon;
+    public Rarity rarity;
+
+    [Header("Fixed Main Effect")]
+    public string mainEffectName;
+    public float mainEffectValue;
+
+    [Header("Possible Combinations")]
+    public List<CombinationRecipe> combinationRecipes;
+
+    [Header("Generated Stats")]
+    public List<SecondaryStat> secondaryStats = new List<SecondaryStat>();
+
+    [System.Serializable]
+    public class SecondaryStat
+    {
+        public StatType type;
+        public float value;
+    }
+
+    [System.Serializable]
+    public class CombinationRecipe
+    {
+        public EquipmentItem item1;
+        public EquipmentItem item2;
+        public EquipmentItem result;
+    }
+
+    public enum StatType
+    {
+        Attack, Defense, Health, HealthRegen,
+        CritChance, CritDamage, DodgeChance, etc
+    }
+}
